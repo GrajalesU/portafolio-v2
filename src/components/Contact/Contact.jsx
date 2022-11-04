ae421f12ff5aa7ceeef2e7c41c6c017cc990e00f
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2022-11-04 14:20:54 -0500

    Add form functionality

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import hired from "../../assets/background/hired.png";
 import wave from "../../assets/background/wave-contact.png";
 import waveD from "../../assets/background/wave-contact-desktop.png";
+import { useForm } from "react-hook-form";
+import emailjs from "emailjs-com";
 
 export default function Contact() {
+  const [messageSended, setMessageSended] = useState(false);
+  const form = useRef();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  function handleEmail() {
+    emailjs
+      .sendForm(
+        "service_yrqlvew",
+        "template_z9cggzt",
+        form.current,
+        "user_nLXDuX67AXZpzs2nKdVKZ"
+      )
+      .then(
+        () => {
+          setMessageSended(true);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  }
+
   return (
     <div className="w-full min-h-screen relative">
       <picture>
@@ -20,50 +48,83 @@ export default function Contact() {
           <h2 className="w-full text-center text-xl font-normal pt-16 mb-11 md:text-2xl">
             CONTÁCTAME
           </h2>
-          <p className="w-10/12 mb-16 mx-auto font-bold text-lg tracking-wide md:text-xl">
-            Envíame un email a{" "}
-            <a
-              href="mailto: juan.grajalesu@gmail.com"
-              className="text-cyan-600 hover:text-cyan-500"
-            >
-              este correo
-            </a>{" "}
-            ó llena el siguiente formulario y estaremos en contacto.
-          </p>
-          <form className="w-4/5 mx-auto text-xl md:text-2xl">
-            <div className="flex flex-col gap-3">
-              <label htmlFor="nombre" className="font-bold">
-                Nombre
-              </label>
-              <input
-                type="text"
-                id="nombre"
-                className="bg-sky-400/[.25] mb-2 p-1"
-              />
-            </div>
-            <div className="flex flex-col gap-3">
-              <label htmlFor="correo" className="font-bold ">
-                Correo
-              </label>
-              <input
-                type="email"
-                id="correo"
-                className="bg-sky-400/[.25] mb-2 p-1"
-              />
-            </div>
-            <div className="flex flex-col gap-3">
-              <label htmlFor="mensaje" className="font-bold">
-                Mensaje
-              </label>
-              <textarea
-                id="mensaje"
-                className="bg-sky-400/[.25] min-h-[200px] mb-14"
-              />
-            </div>
-            <button className="bg-sky-200 w-full mx-auto p-2 rounded-lg text-white font-bold tracking-wider">
-              Enviar
-            </button>
-          </form>
+          {!messageSended ? (
+            <>
+              <p className="w-10/12 mb-16 mx-auto font-bold text-lg tracking-wide md:text-xl">
+                Envíame un email a{" "}
+                <a
+                  href="mailto: juan.grajalesu@gmail.com"
+                  className="text-cyan-600 hover:text-cyan-500"
+                >
+                  este correo
+                </a>{" "}
+                ó llena el siguiente formulario y estaremos en contacto.
+              </p>
+              <form
+                ref={form}
+                className="w-4/5 mx-auto text-xl md:text-2xl"
+                onSubmit={handleSubmit(handleEmail)}
+              >
+                <div className="flex flex-col gap-1 mb-2">
+                  <label
+                    htmlFor="nombre"
+                    className="font-bold"
+                    placeholder="Introduce tu nombre"
+                  >
+                    Nombre
+                  </label>
+                  <input
+                    type="text"
+                    id="nombre"
+                    className="bg-sky-400/[.25] p-1"
+                    {...register("name", { required: true })}
+                  />
+                  {errors.name && (
+                    <small className="text-red-600">Este campo es necesario</small>
+                  )}
+                </div>
+                <div className="flex flex-col gap-1 mb-2">
+                  <label htmlFor="correo" className="font-bold ">
+                    Correo
+                  </label>
+                  <input
+                    type="email"
+                    id="correo"
+                    className="bg-sky-400/[.25] p-1"
+                    {...register("email", { required: true })}
+                  />
+                  {errors.email && (
+                    <small className="text-red-600">Este campo es necesario</small>
+                  )}
+                </div>
+                <div className="flex flex-col gap-1 mb-14">
+                  <label htmlFor="mensaje" className="font-bold">
+                    Mensaje
+                  </label>
+                  <textarea
+                    id="mensaje"
+                    className="bg-sky-400/[.25] min-h-[200px] "
+                    {...register("message", { required: true })}
+                  />
+                  {errors.message && (
+                    <small className="text-red-600">Este campo es necesario</small>
+                  )}
+                </div>
+                <button
+                  type="submit"
+                  className="bg-sky-300 w-full mx-auto p-2 rounded-lg text-white font-bold tracking-wider mb-5 hover:bg-sky-400"
+                >
+                  Enviar
+                </button>
+              </form>
+            </>
+          ) : (
+            <span>
+              Gracias por contactarme. Lo más pronto posible estaré
+              comunicándome contigo, espero que puedan salir buenas
+              oportunidades gracias a esto.
+            </span>
+          )}
         </div>
       </div>
     </div>
--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -3,30 +3,59 @@ import github from "../../assets/icons/GitHub.png";
 import linkedIn from "../../assets/icons/LinkedIn 3.png";
 import resume from "../../assets/icons/Resume.png";
 import line from "../../assets/background/line.png";
+import { motion } from "framer-motion";
 
 export default function Social() {
   return (
     <div
-      className="flex w-full justify-around absolute bottom-5 
+      className="flex w-full justify-around absolute bottom-5 z-20
     md:flex-col md:fixed md:left-[calc(98%-64px)] md:bottom-[12%] md:gap-4"
     >
-      <img src={line} alt="line" className="hidden w-px ml-8 md:block"/>
+      <img src={line} alt="line" className="hidden w-px ml-8 md:block " />
       <a
         href="https://grajalesu.github.io/portfolio/cv_JuanManuelGrajales.pdf"
         target="_blank"
       >
-        <img src={resume} alt="resume" />
+        <motion.img
+          src={resume}
+          alt="resume"
+          className="hover:cursor-pointer"
+          whileHover={{
+            scale: 1.2,
+            transition: { duration: 0.5 },
+          }}
+        />
       </a>
       <a
         href="https://www.linkedin.com/in/juan-manuel-grajales-urquijo/"
         target="_blank"
       >
-        <img src={linkedIn} alt="linkedIn" />
+        <motion.img
+          src={linkedIn}
+          alt="linkedIn"
+          className="hover:cursor-pointer"
+          whileHover={{
+            scale: 1.2,
+            transition: { duration: 0.5 },
+          }}
+        />
       </a>
       <a href="https://github.com/grajalesu" target="_blank">
-        <img src={github} alt="github" />
+        <motion.img
+          src={github}
+          alt="github"
+          className="hover:cursor-pointer"
+          whileHover={{
+            scale: 1.2,
+            transition: { duration: 0.5 },
+          }}
+        />
       </a>
-      <img src={line} alt="line" className="hidden h-1/4 absolute -bottom-[26%] w-px ml-8 md:block"/>
+      <img
+        src={line}
+        alt="line"
+        className="hidden h-1/4 absolute -bottom-[26%] w-px ml-8 md:block"
+      />
     </div>
   );
 }

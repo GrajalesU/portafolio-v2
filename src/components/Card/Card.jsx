61ff83308fd785ce1732ce0ac7f9127af5bcd3e0
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-02-02 00:22:41 -0500

    Fix UI image bg

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 export default function Card({ name, description, img, github, site }) {
   return (
     <article className="w-72 h-[580px] shadow-xl bg-white dark:bg-slate-700 mx-auto mb-3 relative">
-      <div className="w-full h-64 bg-amber-100 flex mb-9">
+      <div className="w-full h-64 bg-amber-100 dark:bg-slate-500 flex mb-9">
         <img
           src={img}
           alt={`preview del proyecto ${name}`}
@@ -10,7 +10,9 @@ export default function Card({ name, description, img, github, site }) {
         />
       </div>
       <h3 className="mb-6 text-center font-bold text-2xl">{name}</h3>
-      <p className="w-4/5 mx-auto font-normal text-sm mb-6 dark:text-slate-300">{description}</p>
+      <p className="w-4/5 mx-auto font-normal text-sm mb-6 dark:text-slate-300">
+        {description}
+      </p>
       <div className="flex flex-row justify-around absolute bottom-2 w-full">
         {github && (
           <button className="p-2 bg-black w-24 text-white rounded hover:bg-black/80">

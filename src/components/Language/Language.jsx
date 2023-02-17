a2e9e4ae31f1c087e2b4d0a44b8cc83c0b6a9f9b
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-02-17 13:25:59 -0500

    Add Language component

---
+++ src/components/Language/Language.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import i18n from "i18next";
+import { motion } from "framer-motion";
+
+export default function Language() {
+  const { language, changeLanguage } = i18n;
+  const handleLanguage = () => {
+    language === "es" ? changeLanguage("en") : changeLanguage("es");
+  };
+  return (
+    <button onClick={handleLanguage} className="flex items-center">
+      <span className="font-bold uppercase text-xl w-3.5 text-center">
+        E
+      </span>
+        <motion.span
+          key={language}
+          animate={{ opacity: 1 }}
+          initial={{ opacity: 0 }}
+          className="font-bold uppercase text-xl w-3 text-center"
+        >
+          {language === "es" ? "S" : "N"}
+        </motion.span>
+    </button>
+  );
+}

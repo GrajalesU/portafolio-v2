fbe3ba2d8f1a4888fe8e03f07d5a39db8946615f
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-02-20 00:49:11 -0500

    Add internationalization

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import Projects from "./components/Projects/Projects";
 import Technologies from "./components/Technologies/Technologies";
+
+import spanish from "./utils/es.json";
+import english from "./utils/en.json";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: english,
+    },
+    es: {
+      translation: spanish,
+    },
+  },
+  lng: "en",
+  fallbackLng: "es",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+
 function App() {
   return (
     <>
